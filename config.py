321959f4638c9ccf66c50e95367fe666bc81c859
drdong654 <vanjdamanj@gmail.com>
2026-05-26 23:22:45 +0300

    Add download directory config

--- config.py
+++ config.py
@@ -1,12 +1,7 @@
-import os
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
 
 TOKEN = os.getenv("TOKEN")
-if not TOKEN:
-    msg = "TOKEN not set in .env"
-    raise ValueError(msg)
-
-DOWNLOAD_DIR = "downloads"
 MAX_FILE_SIZE = 50 * 1024 * 1024
