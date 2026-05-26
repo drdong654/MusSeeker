82d0576a08ec806747d83f11c13074951feab47e
drdong654 <vanjdamanj@gmail.com>
2026-05-27 00:12:54 +0300

    Fix PYTHONPATH imports

--- config.py
+++ config.py
@@ -1,7 +1,12 @@
+from pathlib import Path
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
 TOKEN = os.getenv("TOKEN")
+
 MAX_FILE_SIZE = 50 * 1024 * 1024
+
+DOWNLOAD_DIR = Path("/app/downloads")
+DOWNLOAD_DIR.mkdir(exist_ok=True)
